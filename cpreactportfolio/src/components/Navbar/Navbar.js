e7ad896b4a382f219297b2fc6fc18e18210938cc
Chance Phiniezy <phiniezyc@gmail.com>
2018-02-24 05:10:00 -0500

    cleaned up css

--- cpreactportfolio/src/components/Navbar/Navbar.js
+++ cpreactportfolio/src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import {Navbar, NavItem} from 'react-materialize';
 
 const NavBar = () => (
   <div>
-    <Navbar brand='Chance' right className='grey lighten-1' >
+    <Navbar brand='Chance' left className='grey lighten-1' >
       <NavItem href='/gallery'>Gallery</NavItem>
       <NavItem href='/'>Bio</NavItem>
     </Navbar>
--- cpreactportfolio/src/pages/LandingPage/LandingPage.js
+++ cpreactportfolio/src/pages/LandingPage/LandingPage.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './LandingPage.css'
 
 import  nikePhoto from './xavier-teo-469050-unsplash.jpg';
+import headphonePhoto from './frank-septillion-208829-unsplash.jpg';
 
 const LandingPage = () => (
   <div className='flex-container centerPanel'>
     <div className='Panel1'>
-      <img src={nikePhoto} alt="nikePhoto" className="responsive-img"  />
+      <img src={nikePhoto} alt="nikePhoto" className="responsive-img" id='nikePhoto' />
     </div>
     <div className='Panel2'>
       <h1> Hello </h1>
     </div>
     <div className='Panel3'>
-      <h1> Panel 3 </h1>
+      <img src={headphonePhoto} alt="headphonePhoto" className="responsive-img" id='headPhonePhoto'  />
       
     </div>
   </div>
