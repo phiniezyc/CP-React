63f1a88cfc40961201598ac42133726f582560dc
Chance Phiniezy <phiniezyc@gmail.com>
2018-02-25 02:44:07 -0500

    added in card that pulls in array info and maps over, but isn't filled out yet

--- cpreactportfolio/src/components/MainIntro/MainIntro.js
+++ cpreactportfolio/src/components/MainIntro/MainIntro.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, Dropdown, NavItem } from 'react-materialize';
 import  './MainIntro.css';
 
 import chancePic from './rendiansyah-nugroho-496690-unsplash.jpg';
@@ -7,7 +8,19 @@ const MainIntro = () => (
   <div className='mainIntroDiv'>
     <h2>Chance Phiniezy</h2>
     <img src={chancePic} alt="chancePic" className="circle responsive-img" id='chancePic' />
-    <p>Hi, Chance Full-Stack Developer</p>
+    <h4>Full-Stack Development</h4> 
+    <Dropdown trigger={
+      <Button>Enter Here</Button>
+	}
+    >
+      <NavItem href="/portfolio"> Portfolio</NavItem>
+      <NavItem divider />
+      <NavItem href="/bio" >About Me</NavItem>
+      <NavItem divider />
+      <NavItem href="https://github.com/phiniezyc" >GitHub</NavItem>
+    </Dropdown>
+    <br />
+    
   </div>
 );
 
--- cpreactportfolio/src/pages/GalleryPage/GalleryPage.js
+++ cpreactportfolio/src/pages/GalleryPage/GalleryPage.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Card, CardTitle } from 'react-materialize';
+
+const projectInfoArr = [1, 2];
 
 const GalleryPage = () => (
   <div>
-    <h1>
-        GalleryPage!
-    </h1>
+    {
+      projectInfoArr.map((result) => {
+        return (
+          <Card
+            header={<CardTitle reveal image={result} waves='light' />}
+            title={result}
+            reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}
+          >
+            <p><a href="/">This is a link</a></p>
+          </Card>
+        )
+      })
+    } 
+    
+  
+  
   </div>
 );
 
