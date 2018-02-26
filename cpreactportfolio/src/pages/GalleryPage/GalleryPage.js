49c4677ff1c16143dd268ba4c38b9ed87e10d049
Chance Phiniezy <phiniezyc@gmail.com>
2018-02-26 02:03:35 -0500

    added in portfolio cards w/ buttons that go to show demo and code

--- cpreactportfolio/src/pages/GalleryPage/GalleryPage.js
+++ cpreactportfolio/src/pages/GalleryPage/GalleryPage.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import { Card, CardTitle } from 'react-materialize';
 
-const projectInfoArr = [1, 2];
+
+import ProjectDisplays from '../../components/ProjectDisplays/ProjectDisplays';
+
+
 
 const GalleryPage = () => (
-  <div>
-    {
-      projectInfoArr.map((result) => {
-        return (
-          <Card
-            header={<CardTitle reveal image={result} waves='light' />}
-            title={result}
-            reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}
-          >
-            <p><a href="/">This is a link</a></p>
-          </Card>
-        )
-      })
-    } 
-    
+  <div className='container'>
+    <div>
+      <ProjectDisplays />
+    </div>
   
   
   </div>
