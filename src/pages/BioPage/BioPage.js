8177dba7bee5590079a43889f2c22d9de67c62b6
Chance Phiniezy <phiniezyc@gmail.com>
2018-03-09 05:29:53 -0500

    changed bio phrasing

--- src/pages/BioPage/BioPage.js
+++ src/pages/BioPage/BioPage.js
@@ -36,7 +36,7 @@ const BioPage = () => (
         <br />
         <p>Hi, I am Chance. Welcome to my portfolio collection. Here you will find my projects and other random thoughts running through my head.</p>
         
-        <p>Also serving as a lawyer, and former United States Senate staffer I have been fortunate to live in great cities across the United States, but have settled in my forever home (and hometown), Atlanta, GA.</p>
+        <p>Also serving as a lawyer, and former health care policy staffer, I have been fortunate to live in great cities across the United States, but have settled in my forever home (and hometown), Atlanta, GA.</p>
         
         <p>A true ATLien, I am a long suffering fan of the Braves, Falcons, and Hawks. I do not believe in the Tooth Fairy but I do believe my choice of attire affects the performance of my teams on game days. Irrational superstition is a heck of a drug.</p>
         
