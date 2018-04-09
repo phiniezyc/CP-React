582201ebcb3699ee0aaaa2536596f9bb20f94db1
Chance Phiniezy <phiniezyc@gmail.com>
2018-04-08 21:11:00 -0400

    cleaned up logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import {Navbar, NavItem} from 'react-materialize';
 
 const NavBar = () => (
   <div>
-    <Navbar brand='Chance Phiniezy' left className='grey lighten-1' >
+    <Navbar brand='CP' left className='grey lighten-1' >
       <NavItem href='/'>Home</NavItem>
       <NavItem href='/portfolio'>Portfolio</NavItem>
       <NavItem href='/bio'>Bio</NavItem>
