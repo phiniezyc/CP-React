8413dbdb260345a5115e7b6d13b8e4de6b1cd7e9
Chance Phiniezy <phiniezyc@gmail.com>
2018-05-03 08:19:02 -0400

    added in icons for minimlist look. Revert to last version if issues arise

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Row, Col, Icon} from 'react-materialize';
+
 import './LandingPage.css'
 
 import MainIntro from '../../components/MainIntro/MainIntro';
@@ -6,16 +8,26 @@ import  nikePhoto from './xavier-teo-469050-unsplash.jpg';
 import headphonePhoto from './frank-septillion-208829-unsplash.jpg';
 
 const LandingPage = () => (
-  <div className='flex-container centerPanel'>
-    <div className='Panel1 columnSection'>
-      <img src={nikePhoto} alt="nikePhoto" className="responsive-img" id='nikePhoto' />
+  <div>
+    <div className='flex-container centerPanel'>
+      <div className='Panel1 columnSection'>
+        <img src={nikePhoto} alt="nikePhoto" className="responsive-img" id='nikePhoto' />
+      </div>
+      <div className='Panel2 columnSection'>
+        <MainIntro />
+      </div>
+      <div className='Panel3 columnSection'>
+        <img src={headphonePhoto} alt="headphonePhoto" className="responsive-img" id='headPhonePhoto'  />
+      </div>
     </div>
-    <div className='Panel2 columnSection'>
-      <MainIntro />
-    </div>
-    <div className='Panel3 columnSection'>
-      <img src={headphonePhoto} alt="headphonePhoto" className="responsive-img" id='headPhonePhoto'  />
-    </div>
+    <Row >
+      <Col s={6} className="box valign-wrapper ">
+        <Icon large className="icons ">mood</Icon>
+      </Col>
+      <Col s={6} className="box valign-wrapper">
+        <Icon large className="icons ">mood</Icon>
+      </Col>
+    </Row>
   </div>
 );
 
